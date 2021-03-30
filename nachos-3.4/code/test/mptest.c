659554fe680c1b0f572682fd81796a0970efbe42
Ali Mokhtari <ali.mokhtaary@gmail.com>
2021-03-30 09:53:32 -0500

    local repo loaded

---
+++ nachos-3.4/code/test/mptest.c
@@ -0,0 +1,19 @@
+#include "syscall.h"
+
+int main(){
+    Write("\nExec Matmult 1\n", 20 , ConsoleOutput);
+    Exec("../test/matmult");
+    // Write("\nExec Adder 1\n", 20 , ConsoleOutput);
+    // Exec("../test/adder");
+
+    // Write("\nExec Adder 2\n", 20 , ConsoleOutput);
+    // Exec("../test/adder");
+
+    // //Yield();
+
+    // Write("\nExec Adder 3\n", 20 , ConsoleOutput);
+    // Join(Exec("../test/adder"));
+
+    // Write("\nExec Adder 4\n", 20 , ConsoleOutput);
+    // Exec("../test/adder");
+}
